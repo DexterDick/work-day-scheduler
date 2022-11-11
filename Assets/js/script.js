fdf70c7f448bf483deac9f1b385546982a941295
Dexter Dick <@dexterdick@hotmail.com>
2022-11-11 05:39:35 -0500

    made updates to html and js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -20,4 +20,18 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  $("#currentDay").text(dayjs().format("dddd, MMMM DD"));
+
+  const timeBlocks = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+  $.each(timeBlocks, function (index, value) {
+    $(`<div id="hour-${value}" class="row time-block past">
+    <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
+    <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+    <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+      <i class="fas fa-save" aria-hidden="true"></i>
+    </button>
+  </div>`);
+  });
 });
